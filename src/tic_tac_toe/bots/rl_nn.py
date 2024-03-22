7247d8e469bfd8ca944743c06995d5b94f1b4f0f
CosmicSurgery <mileskeating@gmail.com>
2024-03-22 10:26:29 +0100

    setting up rl_nn

--- src/tic_tac_toe/bots/rl_nn.py
+++ src/tic_tac_toe/bots/rl_nn.py
@@ -1,5 +1,7 @@
 import pickle
 import numpy as np
+from torch import nn
+
 
 
 
@@ -7,10 +9,27 @@ class rl_nn:
     def __init__(self, player):
         self.name = 'rl_nn'
         self.player = player
+        self.dl1 = nn.Linear(18, 36)
+        self.dl2 = nn.Linear(36, 36)
+        self.output_layer = nn.Linear(36, 9)
+
         try:
             self.weights = pickle.load(open(self.name + '_weights.p','rb')) # expected_reward = pickle.load(open('milesbot_expected_reward.p','rb'))
         except:
             print('cant find')
+    
+    def boardToInput(self, board):
+        opponent = 'X' if self.player == 'O' else 'O'
+        
+        board_flat = np.array(board).flatten()
+        player_encoding = np.array([1 if cell == self.player else 0 for cell in board_flat])
+        opponent_encoding = np.array([1 if cell == opponent else 0 for cell in board_flat])
+        
+        nn_input = np.concatenate([player_encoding, opponent_encoding])
+        
+        return nn_input
+
+    
 
     def get_move(self, board):
         pass
